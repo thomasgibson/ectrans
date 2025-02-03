5a339b58f8a9d0de0526fa1ca579b5623a22923d
Thomas Gibson <14180421+thomasgibson@users.noreply.github.com>
2025-02-03 07:39:48 -0800

    OMPGPU: disable async directives

--- src/trans/gpu/internal/trltog_mod.F90
+++ src/trans/gpu/internal/trltog_mod.F90
@@ -492,6 +492,7 @@ CONTAINS
     LLOCAL_CONTRIBUTION = ISENDTOT(MYPROC) > 0
 
 #ifdef OMPGPU
+    ! TODO: Make this asynchronous (pending correctness)
     !$OMP TARGET DATA MAP(TO:IGP_OFFSETS)
 #endif
 #ifdef ACCGPU
@@ -529,6 +530,7 @@ CONTAINS
 #endif
 
 #ifdef OMPGPU
+    ! TODO: Make this asynchronous (pending correctness)
     !$OMP TARGET DATA MAP(PRESENT,ALLOC:PGP) IF(PRESENT(PGP))
     !$OMP TARGET DATA MAP(PRESENT,ALLOC:PGPUV) IF(PRESENT(PGPUV))
     !$OMP TARGET DATA MAP(PRESENT,ALLOC:PGP2) IF(PRESENT(PGP2))
@@ -568,7 +570,7 @@ CONTAINS
       ENDDO
 
 #ifdef OMPGPU
-      ! TODO: Make this asynchronous
+      ! TODO: Make this asynchronous (pending correctness)
       !$OMP TARGET DATA MAP(TO:IFLDA(1:IFLDS))
 #endif
 #ifdef ACCGPU
@@ -582,7 +584,7 @@ CONTAINS
       IIN_TO_SEND_BUFR_V = IIN_TO_SEND_BUFR_OFFSET(MYPROC)
       IF (PRESENT(PGP)) THEN
 #ifdef OMPGPU
-        ! TODO: Make this asynchronous
+        ! TODO: Make this asynchronous (pending correctness)
         !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(2) DEFAULT(NONE) &
         !$OMP&       PRIVATE(JK,JBLK,IFLD,IPOS) &
         !$OMP&       FIRSTPRIVATE(KF_FS,IRECV_WSET_SIZE_V,IRECV_WSET_OFFSET_V, &
@@ -606,7 +608,7 @@ CONTAINS
         ENDDO
       ELSE
 #ifdef OMPGPU
-        ! TODO: Make this asynchronous
+        ! TODO: Make this asynchronous (pending correctness)
         !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(2) DEFAULT(NONE) &
         !$OMP&       PRIVATE(JK,JBLK,IFLD,IPOS) &
         !$OMP&       FIRSTPRIVATE(KF_FS,IRECV_WSET_SIZE_V,IRECV_WSET_OFFSET_V, &
@@ -800,6 +802,7 @@ CONTAINS
     ENDIF
 
 #ifdef OMPGPU
+    ! TODO: Make this asynchronous (pending correctness)
     !$OMP TARGET DATA MAP(PRESENT,ALLOC:ZCOMBUFS) IF(ISEND_COUNTS > 0)
 #endif
 #ifdef ACCGPU
@@ -812,7 +815,7 @@ CONTAINS
       IIN_TO_SEND_BUFR_V = IIN_TO_SEND_BUFR_OFFSET(IPROC)
       ICOMBUFS_OFFSET_V = ICOMBUFS_OFFSET(INS)
 #ifdef OMPGPU
-      ! TODO: Make this asynchronous
+      ! TODO: Make this asynchronous (pending correctness)
       !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(2) DEFAULT(NONE) &
       !$OMP& PRIVATE(IPOS) &
       !$OMP& FIRSTPRIVATE(KF_FS,ILEN,IIN_TO_SEND_BUFR_V,ICOMBUFS_OFFSET_V) &
@@ -945,6 +948,7 @@ CONTAINS
     CALL GSTATS(421,1)
 
 #ifdef OMPGPU
+    ! TODO: Make this asynchronous (pending correctness)
     !$OMP TARGET DATA MAP(PRESENT,ALLOC:ZCOMBUFR) IF(IRECV_COUNTS > 0)
 #endif
 #ifdef ACCGPU
@@ -975,6 +979,7 @@ CONTAINS
       ENDDO
 
 #ifdef OMPGPU
+      ! TODO: Make this asynchronous (pending correctness)
       !$OMP TARGET DATA MAP(TO:IFLDA(1:IRECV_FIELD_COUNT_V))
 #endif
 #ifdef ACCGPU
@@ -985,7 +990,7 @@ CONTAINS
       IRECV_WSET_SIZE_V = IRECV_WSET_SIZE(ISETW)
       IF (PRESENT(PGP)) THEN
 #ifdef OMPGPU
-        ! TODO: Make this asynchronous
+        ! TODO: Make this asynchronous (pending correctness)
         !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(2) DEFAULT(NONE) &
         !$OMP&       PRIVATE(JK,JBLK,IFLD,JI) &
         !$OMP&       FIRSTPRIVATE(IRECV_FIELD_COUNT_V,IRECV_WSET_SIZE_V, &
@@ -1008,7 +1013,7 @@ CONTAINS
         ENDDO
       ELSE
 #ifdef OMPGPU
-        ! TODO: Make this asynchronous
+        ! TODO: Make this asynchronous (pending correctness)
         !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO COLLAPSE(2) DEFAULT(NONE) &
         !$OMP&       PRIVATE(JK,JBLK,IFLD,JI) &
         !$OMP&       FIRSTPRIVATE(IRECV_FIELD_COUNT_V,IRECV_WSET_SIZE_V, &
